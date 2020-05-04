394659c7adb1be9b5434f31746c189482ce24a49
jeremy <jeremytimotius08@gmail.com>
2020-05-04 18:11:47 +0700

    v3

---
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Re‌​ferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.MapHttpAttributeRoutes();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            
+            );
+        }
+    }
+}
---
+++ WebApplication1/Controllers/TestingController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EntityData
+    {
+        public string angka1 { get; set; }
+        public string angka2 { get; set; }
+    }
+        public class TestingController : ApiController
+    {
+        [HttpGet]
+        [ActionName("Perkalian")]
+        public HttpResponseMessage Perkalian()
+        {
+            int angka1;
+            int angka2;
+            int response;
+
+            try
+            {
+                angka1 = 10;
+                angka2 = 2;
+                response = angka1 * angka2;
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "Success",
+                    Hasil = response
+                });
+            }
+            catch
+            {
+                return this.Request.CreateResponse(HttpStatusCode.BadRequest, new
+                {
+                    retVal = "Failed"
+                });
+            }
+        }
+ 
+        
+
+        [HttpPost]
+        [ActionName("Penambahan")]
+        public HttpResponseMessage Penambahan([FromBody] EntityData ED)
+        {
+            int angka1;
+            int angka2;
+            int response;
+
+            try
+            {
+                angka1 = Convert.ToInt32(ED.angka1);
+                angka2 = Convert.ToInt32(ED.angka2);
+                response = angka1 + angka2;
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "Success",
+                    Hasil = response
+                });
+            }
+            catch
+            {
+                return this.Request.CreateResponse(HttpStatusCode.BadRequest, new
+                {
+                    retVal = "Failed"
+                });
+            }
+        }
+
+        
+    }
+}
